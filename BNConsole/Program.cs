ad1c1c39ba4c4ed268f43eb933d07b3ce93bea0c
Slava Osipov <slava.osipov@quantumsoft.ru>
2017-06-07 16:57:59 +0700

    initial commit

---
+++ BNConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BNModel;
+
+namespace BNConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Class1().Print());
+        }
+    }
+}
---
+++ BNModel/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNModel
+{
+    public class Class1
+    {
+        public string Print()
+        {
+            return "Hello";
+        }
+    }
+}
